648b83c1264f0076fbb930dce1ebd89a945e116e
peter <peter@p-ple.com>
2021-01-18 20:39:25 +0900

    [date-picker]  ionic date-picker day scroll customization

--- platforms/android/app/src/main/java/com/plugin/datepicker/ZoeDatePickerDialog.java
+++ platforms/android/app/src/main/java/com/plugin/datepicker/ZoeDatePickerDialog.java
@@ -8,6 +8,10 @@ import android.widget.DatePicker;
 
 public class ZoeDatePickerDialog extends DatePickerDialog {
 
+    private Integer yearSave = null;
+    private Integer monthSave = null;
+    private Integer daySave = null;
+
     public ZoeDatePickerDialog(Context context, int themeResId,
                                OnDateSetListener listener, int year,
                                int monthOfYear, int dayOfMonth) {
@@ -17,7 +21,15 @@ public class ZoeDatePickerDialog extends DatePickerDialog {
     @Override
     public void onDateChanged(DatePicker view, int year, int month, int dayOfMonth) {
         Log.d("onDateChanged", year + "/" + month + "/" + dayOfMonth);
+        if( daySave != null && daySave != 2 && dayOfMonth == 1 ) {
+            super.onDateChanged(view, yearSave, monthSave, dayOfMonth);
+            daySave = dayOfMonth;
+            return ; //ignore this case
+        }
         super.onDateChanged(view, year, month, dayOfMonth);
+        yearSave = year ;
+        monthSave = month ;
+        daySave = dayOfMonth ;
     }
 
     @Override
